9815bee0c8bb74a387e957b997042c1523eaf237
Prasetyo Adi Santoso <prasetyoadisantoso@gmail.com>
2021-10-21 17:03:56 +0800

    Image Upload Function

--- app/Http/Controllers/API/v1/Admin/BookController.php
+++ app/Http/Controllers/API/v1/Admin/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\v1\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -27,14 +28,29 @@ class BookController extends Controller
         $request->validate([
             'title' => 'required|string',
             'author' => 'required|string',
-            'price' => 'required|numeric'
+            'price' => 'required|numeric',
+            'image' => 'required|image',
         ]);
 
-        $payload = $request->all();
+        // Image Processing
+        if ($request->hasFile('image')) {
+            $get_name = $request->file('image')->getClientOriginalName();
+            $get_extension = $request->file('image')->getClientOriginalExtension();
+            $names = md5($get_name) . "." . $get_extension;
+            $imagePath = 'assets/image';
+            Storage::putFileAs('public/' . $imagePath, $request->file('image'), $names);
+            $request->image = $imagePath . "/" . $names;
+        }
 
         try {
-            $response = Book::create($payload);
-            return $this->successResponse($response, 'Sukses simpan data buku');
+            $book = new Book;
+            $book->title = $request->title;
+            $book->author = $request->author;
+            $book->price = $request->price;
+            $book->image  = $request->image;
+            $book->save();
+
+            return $this->successResponse($book, 'Sukses simpan data buku');
         } catch (\Throwable $th) {
             return $this->errorResponse($th->getMessage(), 'Gagal menambahkan data buku');
         }
@@ -52,6 +68,25 @@ class BookController extends Controller
 
         $payload = $request->all();
 
+        // Image Processing
+        if ($request->hasFile('image')) {
+
+            // Get data from car id
+            $book = Book::find($request->id);
+
+            // Delete car image
+            Storage::delete('/public' . '/' . $book->image);
+
+            $get_name = $request->file('image')->getClientOriginalName();
+            $get_extension = $request->file('image')->getClientOriginalExtension();
+            $names = md5($get_name) . "." . $get_extension;
+
+            $imagePath = 'assets/image';
+            Storage::putFileAs('public/' . $imagePath, $request->file('image'), $names);
+
+            $payload['image'] = $imagePath . "/" . $names;
+        }
+
         try {
             $book = Book::find($request->id);
             $response = $book->update($payload);
@@ -68,7 +103,13 @@ class BookController extends Controller
         ]);
 
         try {
-            $response = Book::find($request->id)->delete();
+            $response = Book::find($request->id);
+
+            // Delete car image
+            Storage::delete('/public' . '/' . $response->image);
+
+            $response->delete();
+
             return $this->successResponse($response, 'Sukses hapus data buku');
         } catch (\Throwable $th) {
             return $this->errorResponse($th->getMessage(), 'Gagal hapus data buku');
--- database/migrations/2021_09_21_052224_create_books_table.php
+++ database/migrations/2021_09_21_052224_create_books_table.php
@@ -18,6 +18,7 @@ class CreateBooksTable extends Migration
             $table->string('title');
             $table->string('author');
             $table->integer('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
